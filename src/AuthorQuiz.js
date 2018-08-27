049670c4b5c48c496ac7711a32af5b916a4f4dac
nwilson <nwilson@sumologic.com>
2018-08-27 14:08:39 -0600

    added routing and author add form

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
@@ -77,6 +78,7 @@ function AuthorQuiz({turnData, highlight, onAnswerSelected}){
           <Hero />
           <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
           <Continue />
+          <p><Link to="/add">Add an author</Link></p>
           <Footer />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
 import registerServiceWorker from './registerServiceWorker';
 import {shuffle, sample} from 'underscore';
 
@@ -55,23 +57,51 @@ function getTurnData(authors) {
 
     return {
         books: fourRandomBooks,
-        author: authors.find((author) => author.books.some((title) => title === answer))
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
     }
 }
 
-const state = {
-    turnData: getTurnData(authors),
-    highlight: ''
+function resetState() {
+    return {
+        turnData: getTurnData(authors),
+        highlight: ''
+    };
 }
 
-function onAnswerSelected(answer){
+let state = resetState();
+
+function onAnswerSelected(answer) {
     const isCorrect = state.turnData.author.books.some((book) => book === answer);
     state.highlight = isCorrect ? 'correct' : 'wrong';
     render();
 }
 
+function App() {
+    return <AuthorQuiz {...state}
+                       onAnswerSelected={onAnswerSelected}
+                       onContinue={() => {
+                           state = resetState();
+                           render();
+                       }}/>;
+}
+
+const AuthorWrapper = withRouter(({ history }) =>
+    <AddAuthorForm onAddAuthor={(author) => {
+        authors.push(author);
+        history.push('/');
+    }} />
+);
+
 function render() {
-    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>, document.getElementById('root'));
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.Fragment>
+                <Route exact path="/" component={App} />
+                <Route path="/add" component={AuthorWrapper} />
+            </React.Fragment>
+        </BrowserRouter>, document.getElementById('root'));
 }
 render();
 registerServiceWorker();
